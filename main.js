fb9916e490197a13175c9c8ee0ab276c6713b95c
HJtime <72139168+HJtime@users.noreply.github.com>
2021-01-02 10:59:57 +0900

    JS button active

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ for(let i=1; i<46; i++){
     number.innerHTML=`${i}`;
     numberBox.appendChild(number);
 }
+
+const buttonContainer=document.querySelector('.number__categories');
+
+buttonContainer.addEventListener('click', (event)=>{
+    const active=document.querySelector('.category_btn.active');
+    active.classList.remove('active');
+    const target=event.target.className==='category_btn'? event.target : event.target.parentNode;
+    target.classList.add('active');
+});
